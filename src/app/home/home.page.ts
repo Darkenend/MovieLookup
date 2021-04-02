fe409659c12870217eccdb516cd71ee4fcfe9491
Álvaro Real Gomez <34766606+Darkenend@users.noreply.github.com>
2021-04-02 06:31:43 +0200

    Search Process Implementation
    Added Search Functionality, supporting all pages.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { MovieService } from "../services/movie.service";
+import { HttpClient } from '@angular/common/http';
 import { SearchService } from "../services/search.service";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,75 @@ import { SearchService } from "../services/search.service";
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public searchValue: string;
+  public movies;
+  public current_page = 1;
+  public total_pages = 1;
 
-  constructor(public movieService: MovieService, public searchService: SearchService) {}
+  constructor(public searchService: SearchService, public toastController: ToastController, private http: HttpClient) {}
 
-  public onSearchChange($event) {
-    let queryString = this.searchService.craftQuery($event.detail.value);
-    let results = this.searchService.searchMovies(queryString);
+  private onSearchChange($event) {
+    this.searchValue = $event.detail.value;
+    this.current_page = 1;
+    this.total_pages = 1;
+    this.mainSearch(this.searchValue, this.current_page);
+  }
+
+  private mainSearch(searchValue: string, page: number) {
+    let queryString = this.searchService.craftQuery(searchValue, page);
+    this.searchMovie(queryString);
+  }
+
+  private searchMovie(queryString: string) {
+    this.http.get(queryString, {observe: 'response', responseType: 'json'}).subscribe(data => {
+      this.updateResults(data.body);
+      this.updateList();
+    });
+  }
+
+  private previousPage() {
+    if (this.current_page === 1) {
+      this.notify('There\'s no page 0', 2000);
+    } else {
+      this.current_page--;
+      this.mainSearch(this.searchValue, this.current_page);
+    }
+  }
+
+  private nextPage() {
+    if (this.current_page === this.total_pages) {
+      this.notify('No more movies match', 2000);
+    } else {
+      this.current_page++;
+      this.mainSearch(this.searchValue, this.current_page);
+    }
   }
   
+  private updateResults(data) {
+    console.log("Updating Results...");
+    this.movies = data.results;
+    this.total_pages = data.total_pages;
+    this.printInfo();
+    console.log("Updated Results.");
+  }
+
+  private updateList() {
+    console.log("Updating List...");
+    console.log("Updated List.");
+  }
+
+  private printInfo() {
+    console.log(this.searchValue);
+    console.log(this.movies);
+    console.log(this.current_page);
+    console.log(this.total_pages);
+  }
+
+  async notify(sentMessage: string, sentTime: number) {
+    const toast = await this.toastController.create({
+      message: sentMessage,
+      duration: sentTime
+    });
+    toast.present();
+  }
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { environment } from "../../environments/environment";
-import { Observable, throwError } from "rxjs";
-import { catchError, retry } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +7,9 @@ import { catchError, retry } from "rxjs/operators";
 export class SearchService {
   private SEARCH_PREFIX = 'https://api.themoviedb.org/3/search/movie?';
 
-  constructor(public http: HttpClient) { }
+  constructor() { }
 
-  public craftQuery(qString: string, qLanguage = 'en-US', qPage = 1, qInclude_adult = false, qRegion?: string, qYear?: number, qPrimary_release_year?: number) {
+  public craftQuery(qString: string, qPage = 1, qLanguage = 'en-US', qInclude_adult = false, qRegion?: string, qYear?: number, qPrimary_release_year?: number) {
     let queryInfo: QueryString= {
       api_key: environment.api_key,
       language: qLanguage,
@@ -33,10 +30,6 @@ export class SearchService {
     );
     fullQueryString = fullQueryString.slice(0, -1); // Remove last &
     return fullQueryString;
-  }
-
-  public searchMovies(queryString: string) {
-    this.http.get(queryString, {observe: 'response', responseType: 'json'}).subscribe(data => {console.log(data.body)});
   }
 }
 
@@ -61,52 +54,4 @@ export interface QueryString {
   region?: string,
   year?: number,
   primary_release_year?: number
-}
-
-/**
- * Interface for the search results.
- * @param {number} [page] - Page of the results.
- * @param {MovieResults} [results] - The Movie results themselves, for more information, check the `MovieResults` interface.
- * @param {number} [total_results] - Number of total results.
- * @param {number} [total_pages] - Number of total pages.
- */
-export interface SearchResult extends MovieResults {
-  page?: number,
-  results?: MovieResults,
-  total_results?: number,
-  total_pages?: number
-}
-
-/**
- * Interface for the movie results object inside `SearchResult`.
- * @param {string} [poster_path] - Poster image for the movie, not the full path.
- * @param {boolean} [adult] - Movie contains adult (pornographic) content.
- * @param {string} [overview] - Short description of the movie.
- * @param {string} [release_date] - When was the movie released.
- * @param {Array<number>} [genre_ids] - IDs from the movie's genres.
- * @param {number} [id] - Movie's ID.
- * @param {string} [original_title] - Title in the movie's original language.
- * @param {string} [original_language] - Movie's original language.
- * @param {string} [title] - Movie's title.
- * @param {string} [backdrop_path] - Image for backdrops, not the full path.
- * @param {number} [popularity] - How popular the movie is in TheMovieDB.
- * @param {number} [vote_count] - How many times it's been voted on in TheMovieDB.
- * @param {boolean} [video] - The movie has a video in TheMovieDB.
- * @param {number} [vote_average] - Average score of the movie in TheMovieDB.
- */
-interface MovieResults {
-  poster_path?: string,
-  adult?: boolean,
-  overview?: string,
-  release_date?: string,
-  genre_ids?: Array<number>,
-  id?: number,
-  original_title?: string,
-  original_language?: string,
-  title?: string,
-  backdrop_path?: string,
-  popularity?: number,
-  vote_count?: number,
-  video?: boolean,
-  vote_average?: number
 }
